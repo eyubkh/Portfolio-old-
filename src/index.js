d436a7381a0b2c87a6df3d4758d41e08b4be7db4
eyubkh <eyub.kh@gmail.com>
2022-10-30 19:06:34 +0000

    build: adding 3d models

--- src/index.js
+++ src/index.js
@@ -1,57 +1,48 @@
 import './index.css'
 import resize from './utils/resize'
-import model from './public/test-room2.glb'
+import monitorModel from './public/monitor.glb'
 
 import * as THREE from 'three'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const canvas = window.document.getElementById('canvas')
 const renderer = new THREE.WebGLRenderer({ canvas })
 
+renderer.setClearColor(0xffffff, 1)
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(0, 0, 2)
-camera.lookAt(0, 0, 0)
+camera.position.set(0, 3, 4)
+camera.lookAt(0, 1, 0)
 
 const color = 0xFFFFFF
-const intensity = 0.5
-const ambientLight = new THREE.AmbientLight(color, 0.3)
+const intensity = 0.6
+const ambientLight = new THREE.AmbientLight(color, intensity)
 scene.add(ambientLight)
 
 const light = new THREE.DirectionalLight(color, intensity)
-light.position.set(2, 1, 5)
+light.position.set(0, 1, 5)
 scene.add(light)
 
-const cube = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshPhongMaterial({ color: 'blue' })
-const cubeMesh = new THREE.Mesh(cube, cubeMaterial)
-// scene.add(cubeMesh)
-
-/// Loader
-// const obj = new OBJLoader()
-// obj.load(model, (root) => {
-//   scene.add(root)
-// })
+const plane = new THREE.PlaneGeometry(6, 6, 6)
+const planeMaterial = new THREE.MeshBasicMaterial({ color: '#f1f1f1' })
+const planeMesh = new THREE.Mesh(plane, planeMaterial)
+planeMesh.rotation.x = -Math.PI / 2
+scene.add(planeMesh)
 
 const obj = new GLTFLoader()
-obj.load(model, (gltf) => {
+obj.load(monitorModel, (gltf) => {
   const object = gltf.scene
+  object.scale.set(0.01, 0.01, 0.01)
+  object.position.y = 1.3
   object.rotateY(Math.PI / -2)
-  scene.add(object)
-})
-const obj2 = new GLTFLoader()
-obj2.load(model, (gltf) => {
-  const object = gltf.scene
-  object.rotateY(Math.PI / -2)
-  object.position.set(0, 2, 0)
   scene.add(object)
 })
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.target.set(0, 0, 0)
-controls.update()
+// const controls = new OrbitControls(camera, renderer.domElement)
+// controls.target.set(0, 0, 0)
+// controls.update()
 
 function render () {
   window.requestAnimationFrame(render)
@@ -60,7 +51,7 @@ function render () {
     camera.aspect = canvas.clientWidth / canvas.clientHeight
     camera.updateProjectionMatrix()
   }
-  controls.update()
+  // controls.update()
   renderer.render(scene, camera)
 }
 
