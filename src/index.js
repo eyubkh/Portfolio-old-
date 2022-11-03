19d5fae34f33808fe3872c44faac07dc5a455b71
eyubkh <eyub.kh@gmail.com>
2022-11-03 20:57:41 +0000

    build: adding new 3d model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './index.css'
 import resize from './utils/resize'
 import monitorModel from './public/monitor.glb'
-import booksModel from './public/scene.gltf'
+import booksModel from './public/book.glb'
+import dumbellModel from './public/dumbell.gltf'
+import clockModel from './public/clock.gltf'
 
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
@@ -10,7 +12,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 const canvas = window.document.getElementById('canvas')
 const renderer = new THREE.WebGLRenderer({ canvas })
 
-renderer.setClearColor(0x333333, 1)
+renderer.setClearColor(0xf1f1f1, 1)
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -32,26 +34,50 @@ const planeMesh = new THREE.Mesh(plane, planeMaterial)
 planeMesh.rotation.x = -Math.PI / 2
 scene.add(planeMesh)
 
+const group = new THREE.Group()
+
 const obj = new GLTFLoader()
 obj.load(monitorModel, (gltf) => {
   const object = gltf.scene
   object.scale.set(0.01, 0.01, 0.01)
   object.position.y = 1.3
   object.rotateY(Math.PI / -2)
-  // scene.add(object)
+  group.add(object)
 })
 
 const books = new GLTFLoader()
 books.load(booksModel, (gltf) => {
   const object = gltf.scene
-  console.log(object)
-  object.position.set(0,0,0)
-  scene.add(object)
+  object.scale.set(0.1, 0.1, 0.1)
+  object.position.set(1.5, .73, -.2)
+  object.rotateZ(90)
+  object.rotateY(Math.PI)
+  group.add(object)
 })
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.target.set(0, 0, 0)
-controls.update()
+const dumbell = new GLTFLoader()
+dumbell.load(dumbellModel, (gltf) => {
+  const object = gltf.scene
+  object.position.set(-1.9,.9,0)
+  object.scale.set(7,7,7)
+  object.rotateZ(Math.PI / 4)
+  group.add(object)
+})
+const clock = new GLTFLoader()
+clock.load(clockModel, (gltf) => {
+  const object = gltf.scene
+  object.position.set(-1.5,0,0)
+  object.rotateY(-Math.PI/2)
+  object.scale.set(5,5,5)
+  group.add(object)
+})
+
+group.position.set(.3,0,0)
+scene.add(group)
+
+// const controls = new OrbitControls(camera, renderer.domElement)
+// controls.target.set(0, 0, 0)
+// controls.update()
 
 function render () {
   window.requestAnimationFrame(render)
@@ -60,7 +86,7 @@ function render () {
     camera.aspect = canvas.clientWidth / canvas.clientHeight
     camera.updateProjectionMatrix()
   }
-  controls.update()
+  // controls.update()
   renderer.render(scene, camera)
 }
 
