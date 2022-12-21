0e2c9ec340c528bf63c334b81ad670411bbd5015
eyubkh <eyub.kh@gmail.com>
2022-12-21 15:59:33 +0000

    adding mark spand animation and new env but not good enough

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -3,9 +3,10 @@ import * as THREE from 'three'
 class Marker {
   constructor (position, modelPosition) {
     this.position = position
-    this.geometry = new THREE.SphereGeometry(100, 32, 16)
-    this.material = new THREE.MeshBasicMaterial({ color: 'blue' })
+    this.geometry = new THREE.SphereGeometry(50, 32, 16)
+    this.material = new THREE.MeshStandardMaterial({ color: 'blue' })
     this.mesh = new THREE.Mesh(this.geometry, this.material)
+    this.spandMesh = new THREE.Mesh(this.geometry, new THREE.MeshStandardMaterial({ color: 'blue' }))
 
     this.modelPosition = modelPosition
 
@@ -14,17 +15,26 @@ class Marker {
     this.raycaster = new THREE.Raycaster()
     this.intersects = []
 
+    this.clock = new THREE.Clock()
+    this.delta = 0
+    this.size = 1
+    this.opacity = 1
+
     this.init()
   }
 
   init () {
     window.addEventListener('click', (event) => this.onClick(event))
+    setInterval(() => {
+      this.spandMesh.scale.set(1, 1, 1)
+      this.spandMesh.material.opacity = 1
+      this.size = 1
+      this.opacity = 1
+    }, 6000)
   }
 
   onClick (event) {
     this.intersects = this.raycaster.intersectObjects([this.mesh])
-
-    console.log(this.intersects)
 
     this.pointer.x = (event.clientX / window.innerWidth) * 2 - 1
     this.pointer.y = -(event.clientY / window.innerHeight) * 2 + 1
@@ -33,6 +43,13 @@ class Marker {
   animate (camera) {
     this.raycaster.setFromCamera(this.pointer, camera)
     camera.lookAt(this.modelPosition)
+
+    this.delta = this.clock.getDelta()
+
+    this.size += this.delta * 4
+    this.opacity -= this.delta * 1.5
+    this.spandMesh.scale.set(this.size, this.size, this.size)
+    this.spandMesh.material.opacity = this.opacity
 
     if (this.intersects[0]?.object.uuid === this.mesh.uuid) {
       camera.position.lerp(new THREE.Vector3(0, 1240, 1100), 0.05)
@@ -43,7 +60,9 @@ class Marker {
 
   render (scene) {
     this.mesh.position.copy(this.position)
-    scene.add(this.mesh)
+    this.spandMesh.position.copy(this.position)
+    this.spandMesh.material.transparent = true
+    scene.add(this.mesh, this.spandMesh)
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import * as THREE from 'three'
 import Monitor from './components/Monitor'
 import Environment from './components/Environment'
 
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
+import { LuminosityShader } from 'three/examples/jsm/shaders/LuminosityShader.js'
+import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass'
+
 const webglElement = window.document.getElementById('webgl')
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.setClearColor(0x0c0c0c, 1)
+renderer.setClearColor(0xf1f1f1, 1)
 
 webglElement.appendChild(renderer.domElement)
 
@@ -17,20 +23,39 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.set(0, 5040, 5000)
 camera.lookAt(0, 0, 0)
 
-const color = 'white'
-const intensity = 1.5
+const light = new THREE.HemisphereLight(0xffffff, 0x080820, 1)
+const helper = new THREE.HemisphereLightHelper(light, 500)
+light.position.set(0, 4000, 4000)
+scene.add(helper, light)
 
-const light = new THREE.DirectionalLight(color, intensity)
-const helper = new THREE.DirectionalLightHelper(light, 100, 'blue')
-light.position.set(0, 200, 200)
-scene.add(helper)
-scene.add(light)
+const pointLight = new THREE.PointLight('white', 0.5)
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 300, 'blue')
+pointLight.position.set(0, 2000, 2000)
+scene.add(pointLight, pointLightHelper)
+/// ////
 
-const ambientLight = new THREE.AmbientLight(color, 1.2)
-scene.add(ambientLight)
+const composer = new EffectComposer(renderer)
+const renderPass = new RenderPass(scene, camera)
+composer.addPass(renderPass)
 
-const environment = new Environment(scene)
-environment.init()
+// const bokeh = new BokehPass(scene, camera, {
+//   focus: 500,
+//   aperture: 0,
+//   maxBlur: 0,
+
+//   width: window.innerWidth,
+//   height: window.innerHeight
+// })
+
+// composer.addPass(bokeh)
+
+/// ///
+
+// const ambientLight = new THREE.AmbientLight('white', 1)
+// scene.add(ambientLight)
+
+// const environment = new Environment(scene)
+// environment.init()
 
 const monitor = new Monitor(scene, camera)
 monitor.init()
@@ -39,6 +64,6 @@ function render () {
   window.requestAnimationFrame(render)
 
   monitor.render()
-  renderer.render(scene, camera)
+  composer.render()
 }
 render()
