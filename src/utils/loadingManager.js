058903831244c173ebdab9a5fceb6819c5c7c0d7
eyubkh <eyub.kh@gmail.com>
2023-02-13 19:06:31 +0000

    fixing code for reusability

--- src/utils/loadingManager.js
+++ src/utils/loadingManager.js
@@ -1,20 +1,11 @@
 import { LoadingManager } from 'three'
-import Loading from '../components/Loading'
-
-const loading = new Loading()
 
 export const manager = new LoadingManager()
 
-manager.onStart = async function () {
-  loading.init()
+export function onLoad (callback) {
+  manager.onLoad = callback
 }
 
-manager.onLoad = function () {
-  loading.isLoadDone = true
-  loading.update()
-}
-
-manager.onError = function () {
-  loading.isLoadError = true
-  loading.update()
+export function onError (callback) {
+  manager.onError = callback
 }
