a0e60d55704742e6994a3a91d75ec9cf407b7c83
eyubkh <eyub.kh@gmail.com>
2023-02-13 18:55:51 +0000

    init Desk and terminal component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
-import * as THREE from 'three'
-import Monitor from './components/Monitor'
-import Environment from './components/Environment'
-import Lighting from './components/Lighting'
+import Desk from './Desk'
+import Terminal from './components/Terminal'
+import { onLoad, onError } from './utils/loadingManager'
 
 class App {
-  scene = new THREE.Scene()
-  renderer = new THREE.WebGLRenderer()
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30000)
-  webglElement = window.document.getElementById('webgl')
-
   constructor () {
-    this.monitor = new Monitor(this.scene, this.camera)
-    this.environment = new Environment(this.scene)
-    this.lighting = new Lighting(this.scene)
-
-    this.init()
+    this.terminal = new Terminal()
+    this.desk = new Desk()
   }
 
   init () {
-    this.webglElement.appendChild(this.renderer.domElement)
-
-    this.renderer.setSize(window.innerWidth, window.innerHeight)
-    this.renderer.setClearColor(0xf1f1f1, 1)
-    this.renderer.outputEncoding = THREE.sRGBEncoding
-
-    this.camera.position.set(0, 5040, 5000)
-    this.camera.lookAt(0, 0, 0)
-
-    this.monitor.init()
-    this.environment.init()
-    this.lighting.init()
+    onLoad(this.onLoad.bind(this))
+    onError(this.onError.bind(this))
   }
 
-  render () {
-    window.requestAnimationFrame(() => this.render())
+  onLoad () {
+    this.terminal.isLoadDone = true
+    this.terminal.update()
+  }
 
-    this.renderer.render(this.scene, this.camera)
-    this.monitor.render()
+  onError () {
+    this.terminal.isLoadError = true
+    this.terminal.update()
   }
 }
 
