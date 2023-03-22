1c394ad81653a530ef2dffc84476b5e336f9590f
eyubkh <eyub.kh@gmail.com>
2023-03-22 13:25:55 +0000

    removing unused code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import Desk from './Desk'
 import Terminal from './components/Terminal'
-import { onLoad, onError } from './utils/loadingManager'
 
 class App {
   constructor () {
     this.terminal = new Terminal()
     this.desk = new Desk()
-  }
-
-  init () {
-    onLoad(this.onLoad.bind(this))
-    onError(this.onError.bind(this))
-  }
-
-  onLoad () {
-    this.terminal.isLoadDone = true
-    this.terminal.update()
-    setTimeout(() => {
-      this.desk.start()
-    }, 4000)
-  }
-
-  onError () {
-    this.terminal.isLoadError = true
-    this.terminal.update()
   }
 }
 
