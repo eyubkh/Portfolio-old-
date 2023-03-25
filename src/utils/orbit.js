41b63c12d70d12702fa157f1078e398237f112ee
eyubkh <eyub.kh@gmail.com>
2023-03-25 12:05:15 +0000

    adding features

--- src/utils/orbit.js
+++ src/utils/orbit.js
@@ -2,9 +2,12 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 function orbitControl (camera, domElement) {
   const controls = new OrbitControls(camera, domElement)
-  // controls.maxDistance = 700
-  // controls.minDistance = 100
-  // controls.maxPolarAngle = Math.PI / 2.5
+  controls.maxDistance = 150
+  controls.minDistance = 50
+  controls.maxPolarAngle = Math.PI / 2.5
+  controls.autoRotate = true
+  controls.minAzimuthAngle = Math.Pi / 2
+  controls.autoRotateSpeed = 0.3
 
   return controls
 }
