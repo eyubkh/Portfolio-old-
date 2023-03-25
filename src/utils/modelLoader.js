fb525b85978b6510ca9d4a8ab6c97941128ef63e
eyubkh <eyub.kh@gmail.com>
2023-03-25 12:02:58 +0000

    set rotation function

--- src/utils/modelLoader.js
+++ src/utils/modelLoader.js
@@ -1,17 +1,17 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { manager } from './loadingManager'
+import { loadingManager } from './loadingManager'
 
 class ModelLoader {
   constructor (model) {
     this.model = model
-    this.loader = new GLTFLoader(manager)
+    this.loader = new GLTFLoader(loadingManager)
     this.scale = 10
     this.position = new THREE.Vector3(0, 0, 0)
     this.rotation = new THREE.Euler(0, -Math.PI / 2, 0)
   }
 
-  render (scene) {
+  addTo (scene) {
     this.loader.load(this.model, (gltf) => {
       const object = gltf.scene
       object.rotation.copy(this.rotation)
@@ -23,6 +23,10 @@ class ModelLoader {
 
   setPosition (position) {
     this.position = position
+  }
+
+  setRotation (rotation) {
+    this.rotation = rotation
   }
 
   setScale (scale) {
